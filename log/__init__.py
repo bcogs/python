273ebd1c6c08aa2d18e98882aa56172a21cfd317
bbp-brieuc <bjeunhomme@gmail.com>
2025-05-10 12:01:38 +0200

    initial version of log

---
+++ log/__init__.py
@@ -0,0 +1,210 @@
+import os
+import sys
+import tempfile
+import time
+
+from traceback import format_stack
+from typing import Any
+
+DBG = 0  # or negative for verbose debug
+INFO = 1
+WARN = 2
+ERR = 3
+FATAL = 4
+LEVELS = ("debug", "info", "warning", "error", "fatal")
+
+
+default_logger = None
+
+
+def setup_default(*args, **kwargs):
+    """Initialize the default logger, must be called exactly once to use the module top level logging functions."""
+    global default_logger
+    assert default_logger is None
+    default_logger = logger(file_sink(*args, **kwargs))
+    return default_logger
+
+
+def setup_default_as_ram_sink():
+    """Initialize the default logger with a ram sink and return the sink.
+
+    Meant as a convenience function for unit tests that want to check the content of the logs.
+    Can be called multiple times, resetting the ram sink each time."""
+    global default_logger
+    default_logger = logger(ram_sink())
+    return default_logger
+
+
+def log(*args, **kwargs):
+    """Log at an arbitrary level, args are the same as those of make()."""
+    assert default_logger is not None, "log() was called before setup_default"
+    default_logger.log(*args, **kwargs)
+
+
+def v(*args, **kwargs):
+    """Log at a verbose debug level, the higher the more verbose."""
+    assert default_logger is not None, "v() was called before setup_default"
+    default_logger.v(*args, **kwargs)
+
+
+def dbg(*args, **kwargs):
+    """Log at level debug."""
+    assert default_logger is not None, "dbg() was called before setup_default"
+    default_logger.dbg(*args, **kwargs)
+
+
+def info(*args, **kwargs):
+    """Log at level info."""
+    assert default_logger is not None, "info() was called before setup_default"
+    default_logger.info(*args, **kwargs)
+
+
+def warn(*args, **kwargs):
+    """Log at level warning."""
+    assert default_logger is not None, "warn() was called before setup_default"
+    default_logger.warn(*args, **kwargs)
+
+
+def err(*args, **kwargs):
+    """Log at level error."""
+    assert default_logger is not None, "err() was called before setup_default"
+    default_logger.err(*args, **kwargs)
+
+
+def fatal(*args, **kwargs):
+    """Log at level fatal, which exits by default."""
+    assert default_logger is not None, "fatal() was called before setup_default"
+    default_logger.fatal(*args, **kwargs)
+
+
+class logger(object):
+    """Main logging class."""
+
+    def __init__(self, sink):
+        """Ctor.
+
+        Parameters:
+          sink: any object with a log(self, level, fmt, *args, **kwargs) method,
+                it will be used to actually write the messages; the log() arguments
+                are the same as those of make()
+        """
+        self.sink = sink
+
+    def die(self):
+        """Called after logging at level fatal(), exits the program."""
+        sys.exit(1)
+
+    def log(self, level, fmt, *args, **kwargs):
+        """Log at an arbitrary level, args are the same as those of make()."""
+        self.sink.log(level, fmt, *args, **kwargs)
+        if level >= FATAL:
+            self.die()
+
+    def v(self, level: int, fmt: str, *args: list[Any], **kwargs: dict[str, Any]):
+        """Log at a verbose debug level, the higher the more verbose."""
+        self.log(-level, fmt, *args, **kwargs)
+
+    def dbg(self, fmt, *args: list[Any], **kwargs: dict[str, Any]):
+        """Log at level debug."""
+        self.log(DBG, fmt, *args, **kwargs)
+
+    def info(self, fmt, *args: list[Any], **kwargs: dict[str, Any]):
+        """Log at level info."""
+        self.log(INFO, fmt, *args, **kwargs)
+
+    def warn(self, fmt, *args: list[Any], **kwargs: dict[str, Any]):
+        """Log at level warning."""
+        self.log(WARN, fmt, *args, **kwargs)
+
+    def err(self, fmt, *args: list[Any], **kwargs: dict[str, Any]):
+        """Log at level error."""
+        self.log(ERR, fmt, *args, **kwargs)
+
+    def fatal(self, fmt, *args: list[Any], **kwargs: dict[str, Any]):
+        """Log at level fatal and call die()."""
+        self.log(FATAL, fmt, *args, **kwargs)
+
+
+def make(level: int, fmt: str, *args, **kwargs):
+    """Turn a format and args into a single string, handling bad arguments well.
+
+    Parameters:
+      level: one of DBG, INFO etc or a negative int representing a verbose debug level (the larger the absolute value, the more verbose)
+      fmt: format string
+      args, kwargs: at most one should be non-0, the string will be fmt % that_one
+
+    Returns:
+      a pair (level, message) where the level could be different from the level supplied in argument in case of formatting error
+    """
+    try:
+        msg = fmt % (args if args else kwargs)
+        if level <= FATAL:
+            return level, msg
+        level, msg = FATAL, ("(invalid log level %d) " % level) + msg
+    except Exception:
+        level = max(min(level, FATAL), ERR)
+        msg = f"log format error - level={level!r}, fmt={fmt!r}, args={args!r}, kwargs={kwargs!r}"
+    caller_stack = "".join(format_stack(limit=10)[:-1])
+    return level, msg + "\n at " + caller_stack
+
+
+class file_sink(object):
+    """Default sink that logs to files and stderr.
+
+    It's also a context manager, it flushes and closes any open file when exiting.
+    """
+
+    def __init__(self, file_path=None, stderr_level=WARN, file_level=DBG):
+        self.stderr_level, self.file_level = stderr_level, file_level
+        if file_path is None:
+            self.file = tempfile.NamedTemporaryFile(
+                dir=tempfile.gettempdir(),
+                delete=False,
+                prefix=os.path.basename(sys.argv[0]) + "." + str(os.getpid()) + "." + time.strftime("%y%m%d-%H%M%S"),
+            )
+            return
+        self.file = open(file_path, "ab") if file_path else None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self._close()
+
+    def __del__(self):
+        self._close()
+
+    def _close(self):
+        if not self.__dict__.get("file", None):
+            return
+        try:
+            self.file.flush()
+        except BaseException:
+            pass
+        try:
+            self.file.close()
+        except BaseException:
+            pass
+        self.file = None
+
+    def log(self, level, fmt, *args, **kwargs):
+        """Log a message, args are the same as those of make()."""
+        if level < self.stderr_level and level < self.file_level:
+            return
+        level, msg = make(level, fmt, *args, **kwargs)
+        msg = LEVELS[max(level, 0)][0].upper() + time.strftime("%m%d %H:%M:%S ") + msg + "\n"
+        if level >= self.stderr_level:
+            sys.stderr.write(msg)
+        if level >= self.file_level and self.file:
+            self.file.write(msg.encode("utf-8", errors="surrogateescape"))
+            self.file.flush()
+
+
+class ram_sink(object):
+    """In memory sink, useful for unit tests that want to check log content."""
+
+    def __init__(self):
+        self.logs = []  # (level, message) pairs
+
+    def log(self, level, fmt, *args, **kwargs):
+        self.logs.append(make(level, fmt, *args, **kwargs))
---
+++ log/test.py
@@ -0,0 +1,254 @@
+# to run the tests:   python3 -m unittest test.py
+import contextlib
+import io
+import os
+import re
+import shutil
+import tempfile
+import unittest
+
+import __init__ as log
+
+
+class TestFileSink(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
+
+    def test_log_to_file(self):
+        for file_level in (-2, log.DBG, log.WARN):
+            fpath = os.path.join(self.test_dir, "file_level%d" % file_level)
+            with log.file_sink(file_path=fpath, stderr_level=log.FATAL + 1, file_level=file_level) as sink:
+                for level in range(-4, log.FATAL):
+                    sink.log(level, "foo %d", level)
+            with open(fpath) as f:
+                nlines = 0
+                for line in f:
+                    nlines += 1
+                    assert line.endswith("\n")
+                    c, i = re.search(r"^(.)\d\d\d\d \d\d:\d\d:\d\d foo (-?\d+)$", line).groups()
+                    self.assertGreaterEqual(int(i), file_level)
+                self.assertEqual(log.FATAL - file_level, nlines)
+
+    def test_log_only_to_stderr(self):
+        sink = log.file_sink(stderr_level=log.INFO, file_path="")
+        buf = io.StringIO()
+        with contextlib.redirect_stderr(buf):
+            sink.log(log.DBG, "debug")
+            sink.log(log.INFO, "info %d", 6)
+            sink.log(log.WARN, "warn %(foo)s", foo="bar")
+            sink.log(log.ERR, "invalid *args %s", "foo", "bar")
+        stderr = buf.getvalue()
+        # print('------------------------------------------------------\n' + stderr)
+        self.assertTrue(
+            re.search(
+                "\n".join(
+                    (
+                        r"^I.*info 6",
+                        r"^W.*warn bar",
+                        r"^E.*format.*invalid \*args.*foo.*bar.*log/test\.py.*line \d+",
+                    )
+                ),
+                stderr,
+                re.MULTILINE | re.DOTALL,
+            )
+        )
+
+    def log_to_both_file_and_stderr(self):
+        fpath = os.path.join(self.test_dir, "foo")
+        with log.file_sink(stderr_level=log.INFO, file_path=fpath) as sink:
+            buf = io.StringIO()
+            with contextlib.redirect_stderr(buf):
+                sink.log(log.WARN, "warning")
+            self.assertIn("warning", buf.getvalue())
+        with open(fpath) as f:
+            self.assertIn("warning", f.read())
+
+    def test_it_does_not_go_bananas_when_logging_binary_rubbish(self):
+        fpath = os.path.join(self.test_dir, "foo")
+        with log.file_sink(stderr_level=log.INFO, file_path=fpath) as sink:
+            buf = io.StringIO()
+            with contextlib.redirect_stderr(buf):
+                sink.log(log.ERR, "binary \xd4 rubbish")
+            self.assertIn("rubbish", buf.getvalue())
+        with open(fpath) as f:
+            self.assertIn("rubbish", f.read())
+
+    def test_various_log_args(self):
+        fpath = os.path.join(self.test_dir, "foo")
+        with log.file_sink(file_path=fpath, stderr_level=log.FATAL + 1) as sink:
+            sink.log(log.DBG, "valid *args %s %d", "foo", -5)
+            sink.log(log.INFO, "valid **kwargs %(foo)s %(bar)d", foo="foo", bar=3)
+            sink.log(log.WARN, "invalid *args %s %s %s", "foo")
+            sink.log(log.WARN, "invalid **kwargs %(foo)s", bar="baz")
+            sink.log(log.FATAL, "invalid empty *args %s %d")
+            sink.log(log.FATAL, "invalid empty **kwargs %(foo)s")
+            sink.log(log.FATAL + 1, "invalid level")
+        with open(fpath) as f:
+            s = f.read()
+        # print('------------------------------------------------------\n' + s)
+        self.assertTrue(
+            re.search(
+                "\n".join(
+                    (
+                        r"^D\d\d\d\d \d\d:\d\d:\d\d valid \*args foo -5",
+                        r"^I\d\d\d\d \d\d:\d\d:\d\d valid \*\*kwargs foo 3",
+                        r"^E\d\d\d\d \d\d:\d\d:\d\d log format error.*invalid \*args %s %s %s.*foo.*log/test\.py.*line \d+.*",
+                        r"^E\d\d\d\d \d\d:\d\d:\d\d log format error.*invalid \*\*kwargs.*foo.*bar.*baz.*log/test\.py.*line \d+.*",
+                        r"^F\d\d\d\d \d\d:\d\d:\d\d log format error.*invalid empty \*args %s %d.*log/test\.py.*line \d+.*",
+                        r"^F\d\d\d\d \d\d:\d\d:\d\d log format error.*invalid empty \*\*kwargs.*foo.*log/test\.py.*line \d+.*",
+                        r"^F\d\d\d\d \d\d:\d\d:\d\d .*invalid log level \d+.*invalid level.*log/test\.py.*line \d+.*",
+                    )
+                ),
+                s,
+                re.MULTILINE | re.DOTALL,
+            )
+        )
+
+
+class TestRamSink(unittest.TestCase):
+    def test_valid_args(self):
+        sink = log.ram_sink()
+        sink.log(log.INFO, "info")
+        sink.log(log.WARN, "warn %d", 5)
+        sink.log(log.ERR, "err %(foo)s", foo="bar")
+        self.assertEqual(
+            [
+                (log.INFO, "info"),
+                (log.WARN, "warn 5"),
+                (log.ERR, "err bar"),
+            ],
+            sink.logs,
+        )
+
+    def test_invalid_args(self):
+        sink = log.ram_sink()
+        sink.log(log.DBG, "dbg", "foo")
+        self.assertRegex(sink.logs[0][1], "dbg.*foo")
+        sink.log(log.INFO, "info %(foo)s")
+        self.assertRegex(sink.logs[1][1], "info.*foo")
+
+
+class TestMake(unittest.TestCase):
+    def test_valid(self):
+        self.assertEqual((log.INFO, "foo"), log.make(log.INFO, "foo"))
+        self.assertEqual((log.WARN, "foo bar 4"), log.make(log.WARN, "foo %s %d", "bar", 4))
+        self.assertEqual((-5, "foo bar 4"), log.make(-5, "foo %(foo)s %(bar)d", foo="bar", bar=4))
+
+    def test_invalid_args(self):
+        logger, m = log.make(log.INFO, "invalid args %s", "foo", 42)
+        self.assertEqual(log.ERR, logger)
+        self.assertIn("log format error", m)
+        self.assertIn("invalid args", m)
+        self.assertIn("foo", m)
+        self.assertIn("42", m)
+
+    def test_invalid_kwargs(self):
+        logger, m = log.make(log.INFO, "invalid kwargs %(a)s %(b)s", a="foo", z=42)
+        self.assertEqual(log.ERR, logger)
+        self.assertIn("log format error", m)
+        self.assertIn("invalid kwargs", m)
+        self.assertIn("foo", m)
+        self.assertIn("42", m)
+
+    def test_invalid_level(self):
+        for level in range(log.FATAL + 1, log.FATAL + 3):
+            logger, m = log.make(level, "foo %(bar)s", bar="baz")
+            self.assertEqual(log.FATAL, logger)
+            self.assertIn("invalid log level %d" % level, m)
+            self.assertIn("foo baz", m)
+
+    def test_invalid_everything(self):
+        logger, m = log.make(log.FATAL + 1, "foo", "bar")
+        self.assertEqual(log.FATAL, logger)
+        self.assertIn("foo", m)
+        self.assertIn("bar", m)
+
+
+class TestLogger(unittest.TestCase):
+    def test_die(self):
+        with self.assertRaises(SystemExit):
+            log.logger(log.ram_sink()).die()
+
+    def test_log_functions(self):
+        rl = log.logger(log.ram_sink())
+        for x in (
+            ("log", log.WARN, log.WARN),
+            ("v", 3, -3),
+            ("dbg", log.DBG),
+            ("info", log.INFO),
+            ("warn", log.WARN),
+            ("err", log.ERR),
+            ("fatal", log.FATAL),
+        ):
+            method = getattr(rl, x[0])
+            args = list(x[1 : len(x) - 1])
+            level = x[len(x) - 1]
+            if level < log.FATAL:
+                method(*(args + ["foo"]))
+            else:
+                with self.assertRaises(SystemExit):
+                    method(*(args + ["foo"]))
+            self.assertEqual(level, rl.sink.logs[len(rl.sink.logs) - 1][0])
+            self.assertEqual("foo", rl.sink.logs[len(rl.sink.logs) - 1][1])
+            if level < log.FATAL:
+                method(*(args + ["bar %(a)d %(b)s"]), a=2, b="blah")
+            else:
+                with self.assertRaises(SystemExit):
+                    method(*(args + ["bar %(a)d %(b)s"]), a=2, b="blah")
+            self.assertEqual(level, rl.sink.logs[len(rl.sink.logs) - 1][0])
+            self.assertEqual("bar 2 blah", rl.sink.logs[len(rl.sink.logs) - 1][1])
+            if level < log.FATAL:
+                method(*(args + ["baz %s %d"]), "blah", -3)
+            else:
+                with self.assertRaises(SystemExit):
+                    method(*(args + ["baz %s %d"]), "blah", -3)
+            self.assertEqual(level, rl.sink.logs[len(rl.sink.logs) - 1][0])
+            self.assertEqual("baz blah -3", rl.sink.logs[len(rl.sink.logs) - 1][1])
+            if level < log.FATAL:
+                method(*(args + ["invalid %d"]))
+            else:
+                with self.assertRaises(SystemExit):
+                    method(*(args + ["invalid %d"]))
+            self.assertEqual(max(level, log.ERR), rl.sink.logs[len(rl.sink.logs) - 1][0])
+            self.assertIn("log format error", rl.sink.logs[len(rl.sink.logs) - 1][1])
+            self.assertIn("invalid %d", rl.sink.logs[len(rl.sink.logs) - 1][1])
+            self.assertIn("", rl.sink.logs[len(rl.sink.logs) - 1][1])
+            self.assertRegex(rl.sink.logs[len(rl.sink.logs) - 1][1], "log/test\.py.*line \d+")
+
+
+class TestDefault(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
+
+    def test_default_logging(self):
+        dl = log.setup_default(stderr_level=log.FATAL + 1)
+        assert dl.sink is not None
+        assert dl.sink.file is not None
+        file_path = dl.sink.file.name
+        try:
+            log.v(1, "verbose")
+            log.dbg("dbg")
+            log.info("info %d", 2)
+            log.warn("warn %(foo)s", foo="foo")
+            log.err("err")
+            with self.assertRaises(SystemExit):
+                log.fatal("fatal")
+            with open(file_path) as f:
+                s = f.read()
+                self.assertNotIn("verbose", s)
+                self.assertIn("dbg", s)
+                self.assertIn("info 2", s)
+                self.assertIn("warn foo", s)
+                self.assertIn("err", s)
+                self.assertIn("fatal", s)
+            # check default logging can be setup only once
+            with self.assertRaises(AssertionError):
+                log.setup_default()
+        finally:
+            os.remove(file_path)
