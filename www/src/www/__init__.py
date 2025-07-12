4bbf72244da920e1309b30ca2f5ba8d2552735be
bbp-brieuc <bjeunhomme@gmail.com>
2025-07-12 19:08:23 +0200

    log retries

--- www/src/www/__init__.py
+++ www/src/www/__init__.py
@@ -3,12 +3,12 @@ import requests.adapters
 import time
 
 
-class _retrying_http_adapter(requests.adapters.HTTPAdapter):
+class _retrying_http_adapter(requests.adapters.HTTPAdapter, logger=None):
     _sleep = time.sleep
 
-    def __init__(self, max_retries: int = 0, backoff0_seconds: float = 0.5):
+    def __init__(self, max_retries: int = 0, backoff0_seconds: float = 0.5, logger=None):
         super().__init__()
-        self.max_retries, self.backoff0_seconds = max_retries, backoff0_seconds
+        self.max_retries, self.backoff0_seconds, self._logger = max_retries, backoff0_seconds, logger
 
     def send(self, request, **kwargs):
         "part of the HTTPAdapter interface"
@@ -17,7 +17,14 @@ class _retrying_http_adapter(requests.adapters.HTTPAdapter):
             response = super().send(request, **kwargs)
             if (response.status_code // 100 == 2) or (i >= self.max_retries):
                 return response
-            _retrying_http_adapter._sleep(backoff_seconds)
+            if self._logger:
+                self._logger.warn(
+                    "request to %s returned %d %r, waiting %r seconds and retrying",
+                    request.url,
+                    response.status_code,
+                    response.reason,
+                )
+            _retrying_http_adapter._sleep(backoff_seconds, logger=self._logger)
             i += 1
             backoff_seconds *= 2
 
@@ -42,10 +49,10 @@ class session(requests.Session):
         return resp
 
 
-def new_session(max_retries: int = 0, backoff0_seconds: float = 0.5) -> requests.Session:
+def new_session(max_retries: int = 0, backoff0_seconds: float = 0.5, logger=None) -> requests.Session:
     "create a new session that retries faileds requests up to max_retries times with exponential backoff starting at backoff0_seconds"
     sess = session()
-    adapter = _retrying_http_adapter(max_retries=max_retries, backoff0_seconds=backoff0_seconds)
+    adapter = _retrying_http_adapter(max_retries=max_retries, backoff0_seconds=backoff0_seconds, logger=logger)
     sess.mount("http://", adapter)
     sess.mount("https://", adapter)
     return sess
