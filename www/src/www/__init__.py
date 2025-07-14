84ea59375e7f5532c6fd50a4358e74ca2567178c
bbp-brieuc <72989042+bbp-brieuc@users.noreply.github.com>
2025-07-14 09:34:05 +0200

    allow to override the sleep function in new_session (#30)

--- www/src/www/__init__.py
+++ www/src/www/__init__.py
@@ -4,11 +4,9 @@ import time
 
 
 class _retrying_http_adapter(requests.adapters.HTTPAdapter):
-    _sleep = time.sleep
-
-    def __init__(self, max_retries: int = 0, backoff0_seconds: float = 0.5, logger=None):
+    def __init__(self, max_retries: int = 0, backoff0_seconds: float = 0.5, logger=None, sleep=time.sleep):
         super().__init__()
-        self.max_retries, self.backoff0_seconds, self._logger = max_retries, backoff0_seconds, logger
+        self.max_retries, self.backoff0_seconds, self._logger, self.sleep = max_retries, backoff0_seconds, logger, sleep
 
     def send(self, request, **kwargs):
         "part of the HTTPAdapter interface"
@@ -25,7 +23,7 @@ class _retrying_http_adapter(requests.adapters.HTTPAdapter):
                     response.reason,
                     backoff_seconds,
                 )
-            _retrying_http_adapter._sleep(backoff_seconds)
+            self.sleep(backoff_seconds)
             i += 1
             backoff_seconds *= 2
 
@@ -50,10 +48,12 @@ class session(requests.Session):
         return resp
 
 
-def new_session(max_retries: int = 0, backoff0_seconds: float = 0.5, logger=None) -> requests.Session:
+def new_session(max_retries: int = 0, backoff0_seconds: float = 0.5, logger=None, sleep=time.sleep) -> requests.Session:
     "create a new session that retries faileds requests up to max_retries times with exponential backoff starting at backoff0_seconds"
     sess = session()
-    adapter = _retrying_http_adapter(max_retries=max_retries, backoff0_seconds=backoff0_seconds, logger=logger)
+    adapter = _retrying_http_adapter(
+        max_retries=max_retries, backoff0_seconds=backoff0_seconds, logger=logger, sleep=sleep
+    )
     sess.mount("http://", adapter)
     sess.mount("https://", adapter)
     return sess
